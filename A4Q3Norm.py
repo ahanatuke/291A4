9bac33ab7d0b2a3c96b3c91a9171d04cfaadcb63
Anya Hanatuke <anyahanatuke@gmail.com>
2023-04-03 13:02:18 -0600

    Q3 is done for norm

--- A4Q3Norm.py
+++ A4Q3Norm.py
@@ -4,62 +4,33 @@ from A4utils import connectPort
 # for each songwriter along with the songwriter’s id.
 
 def Q3():
-    #client = connectPort()
-    client = MongoClient() #todo fix me for hand in
+    client = connectPort()
 
     db = client['A4dbNorm']
     swCol = db['songwriters']
 
-    '''  results = swCol.aggregate(
-        [
-            {"$match":
-                 {"recordings" : {"$not" : {"$size" : 0}} }
-             },
-            {
-            "$project" :
-                {
-                    "_id" : "$_id" ,
-                    "total_length" : {"$sum" : "$recordings"},
-                   # "songwriter_id" : {"$first" : "$songwriter_id"},
-                    #"songwriter": {"$first": "$name"},
-                    "num_records": {"$size":"$recordings"},
-                    "recordings":1
-
-                }
-                $lookup:
-    #{
-    #    from: <collection to join>,
-    #    localField: <field from the input documents>,
-    #     foreignField: <field from the documents of the "from" collection>,
-    #    as: <output array field>
-    #   }
-        }]
-    )'''
-
     results = swCol.aggregate(
-        [{
-            "$unwind" :
-                {"$recordings"}
-        },
-        {
-            "$lookup" : {
-                "from" : "recordings" ,
-                "localField" : "recordings",
-                "foreignField" : "recording_id",
-                "as" : "recordings"
-            }
-        },
-        {
-            "$group" :
-                {
-                    "_id" : "$songwriter_id",
-                    #"songwriter_id" : {"$first" : "$songwriter_id"},
-                    "total_length" : {"$sum" : "$recordings.length"},
-                    "plsplspls" : {"$first" : "$recordings.length"}
+        [
+            {
+                '$lookup': {
+                    'from': 'recordings',
+                    'localField': 'recordings',
+                    'foreignField': 'recording_id',
+                    'as': 'recordings'
                 }
-        }]
+            },
+            {
+                '$unwind': '$recordings'
+            },
+            {
+                '$group': {
+                    '_id' : '$_id',
+                    'total_length': {'$sum': '$recordings.length'},
+                    'songwriter_id': {'$first': '$songwriter_id'}
+                }
+            }
+        ]
     )
-
 
     for i in list(results):
         print(i)
