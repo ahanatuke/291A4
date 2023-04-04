340f0fc94ea898d2401e8edd6fa3ac81602a86ba
Anya Hanatuke <anyahanatuke@gmail.com>
2023-04-04 10:54:49 -0600

    A4Q2 done

---
+++ A4Q2Embed.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+from A4utils import connectPort
+# Q2: Write a query to get the average rhythmicality
+# for all recordings that have a recording_id beginning
+# with “70”.
+
+
+def Q2():
+    client = connectPort()
+    db = client['A4dbEmbed']
+    collection = db['songwritersRecords']
+
+    results = collection.aggregate(
+        [
+            {
+                "$unwind" : "$recordings"
+            },
+            {"$match":
+                 {"recordings.recording_id" : {"$regex" :"^70"} }
+            },
+            {
+                "$group":
+                    {
+                        "_id": " ",
+                        "avg_rhythmicality": {"$avg": "$recordings.rhythmicality"},
+                    }
+            }]
+    )
+
+    for i in list(results):
+        print(i)
+
+    #results = collection.find()
+    print(list(results))
+
+    client.close()
+
+Q2()
